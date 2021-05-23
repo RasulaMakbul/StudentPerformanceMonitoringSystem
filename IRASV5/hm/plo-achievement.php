671eededb204c0c2a8b30c344c9ba6d900c68be8
RasulaMakbul <emu.cfs@gmail.com>
2021-05-23 13:03:58 +0600

    system front end

---
+++ IRASV5/hm/plo-achievement.php
@@ -0,0 +1,17 @@
+<?php
+    require 'php/middleware.php';
+    require 'php/mysql.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>plo-achivement</title>
+</head>
+<body>
+    <dic class="container"><h1>webpage under process </h1></dic>
+</body>
+</html>
